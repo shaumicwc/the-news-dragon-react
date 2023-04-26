4ed86ec249fa7bc01391e97ee081029628ae19c8
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-04-27 01:03:26 +0600

    React Marquee and Responsive NavBar

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
+
+
 
 const Header = () => {
     return (
-        <div>
-            <h1>heda</h1>
-        </div>
+        <Container>
+            <div className="text-center">
+                <img src={logo} alt="" />
+                <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
+                <p>{moment().format("dddd, MMMM Do YYYY hh:mm a")}</p>
+            </div>
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>{' '}
+                <Marquee className='text-danger' speed={65}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+
+            <div>
+                <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                    <Container>
+                        
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="mx-auto">
+                                <Nav.Link href="#features">Home</Nav.Link>
+                                <Nav.Link href="#pricing">About</Nav.Link>
+                                <Nav.Link href="#pricing">Career</Nav.Link>
+                               
+                            </Nav>
+                            <Nav>
+                                <Nav.Link href="#deets">Profile</Nav.Link>
+                                <Nav.Link eventKey={2} href="#memes">
+                                <Button variant="secondary">Login</Button>
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        </Container>
     );
 };
 
-export default Header;<h1>heda</h1>
+export default Header;
