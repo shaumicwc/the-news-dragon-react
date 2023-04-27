0a3b4a7a051a0b3f1421ac1892c535ee2c1f505a
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-04-27 08:25:27 +0600

    right nav updated

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
 
 const RightNav = () => {
     return (
+
         <div>
-            <h2>Right Nav</h2>
+            <div>
+                <h4>Login With</h4>
+                <Button className='mb-2' variant="outline-primary"> <FaGoogle /> Login with Google </Button>
+                <Button variant="outline-secondary">
+                    <FaGithub /> Login with Github</Button>
+            </div>
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item> <FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter />Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram />Instagram</ListGroup.Item>
+
+                </ListGroup>
+            </div>
+            <QZone/>
         </div>
+
+
+
+
     );
 };
 
