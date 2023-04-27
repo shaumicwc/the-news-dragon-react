d2275310acdc4a2f4cd5898a9e7aaa012fea532d
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-04-27 13:11:38 +0600

    left nav updated

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+
+    const [catagories, setCatagories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/catagories')
+            .then(res => res.json())
+            .then(data => setCatagories(data))
+            .catch(error => console.error(error))
+    }, [])
+
     return (
         <div>
-            <h2>Left Nav</h2>
+            <h4>All Category</h4>
+            <div className='ps-4 '>
+                {
+                    catagories.map(catagoey =>
+                        <p
+                            key={catagoey.id}
+                        >
+                            <Link to={`/catagoey/${catagoey.id}`} className='text-decoration-none text-black'>{catagoey.name}</Link>
+
+                        </p>)
+                }
+            </div>
         </div>
     );
 };
