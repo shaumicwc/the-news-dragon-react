54b8fd8231736caf2e89e8ec6b25d3f798db9999
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-05-01 05:16:48 +0600

    code updated

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`https://the-news-dragon-server-jhankarphero.vercel.app/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://react-news-dragon-server-shaumicwc.vercel.app/categories/${params.id}`)
             }
         ]
     }, 
@@ -52,7 +52,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`https://the-news-dragon-server-jhankarphero.vercel.app/news/${params.id}`)
+                loader: ({params}) => fetch(`https://react-news-dragon-server-shaumicwc.vercel.app/news/${params.id}`)
             }
         ]
     }
